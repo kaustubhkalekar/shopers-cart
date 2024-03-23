633645ba84af6ac5bdbad97291bce0b1bc1a12e1
kaustubhkalekar <kasti.kalekar@gmail.com>
2024-03-23 12:24:16 +0530

    changes to deploy to production

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express';
 import dotenv from'dotenv';
 import cookieParser from 'cookie-parser';
@@ -21,16 +22,28 @@ app.use(express.urlencoded({extended:true}))
 // CookieParser Middlewar
 app.use(cookieParser())
 
-app.get('/', (req, res)=>{
-    res.send("APi is running")
-})
-
 
 app.use('/api/products',productsRoutes);
 app.use('/api/users/',userRoutes);
 app.use('/api/orders/',orderRoutes);
 app.get('/api/config/paypal',(req,res)=>res.send({clientId: process.env.PAYPAL_CLIENT_ID}))
 
+
+const __dirname = path.resolve()
+if(process.env.NODE_ENV === 'production'){
+    
+    // set static folder
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    // any route that is not api will be redirected to index.html
+    app.get('*', (req,res)=>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', index.html))
+    );
+}else{
+    app.get('/', (req, res)=>{
+        res.send("APi is running")
+    })
+}
 
 app.use(notFound);
 app.use(errorHandler)
